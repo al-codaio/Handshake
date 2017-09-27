cd282ea84071c7694705123b23a1cd6464fceea3
Jandrews <jordandrews@hotmail.com>
2017-09-27 16:36:48 -0500

    Updates to Handshake

--- contracts/Handshake.sol
+++ contracts/Handshake.sol
@@ -1,31 +1,34 @@
 pragma solidity ^0.4.15;
 
 import "./LaborContract.sol";
+import "./interfaces/HandshakeI.sol";
 import "./Owned.sol";
 
-contract Handshake is Owned {
+contract Handshake is Owned, HandshakeI {
 
   address[] public laborContracts;
   mapping(address => bool) public laborContractExists;
 
-  // Emitted on creation of contracts - could be listened to by a regulating body
-  event LaborContractCreated(bytes32 indexed uwcLocation, address indexed atAddress);
+  address[] public agencies;
+  mapping(address => bool) public agencyExists;
+
+  event LogLaborContractCreated(string indexed data, address indexed agency, address indexed atAddress);
 
   // Anything else needs to happen on creation?
   // Any relation to accreditation etc.via uPort?
-  function RecruitmentAgency() {}
+  function Handshake() {}
 
-  // Should we check that uwcLocation hasn't been used by other contracts?
-  function newLaborContract(bytes32 uwcLocation)
-  public
-  fromOwner
-  returns (address) {
-    LaborContract laborContract = new LaborContract(uwcLocation);
+  function createLaborContract(string data) public returns(address){
+    require(agencyExists[msg.sender]);
+    LaborContract laborContract = new LaborContract(data);
     laborContracts.push(laborContract);
     laborContractExists[laborContract] = true;
-    LaborContractCreated(uwcLocation, laborContract);
+    LogLaborContractCreated(data, msg.sender, laborContract);
     return laborContract;
   }
 
-
+  function registerAgency(address agency) public returns(bool);
+  function isRegistered(address agency) public returns(bool);
+  /*For GUI iteration*/
+  function getLaborContractAt(uint index) public returns(address);
 }
