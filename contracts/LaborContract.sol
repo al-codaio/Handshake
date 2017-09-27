baca59d05471d756a6f43e4d4085aa07ffe22a6b
Jandrews <jordandrews@hotmail.com>
2017-09-27 16:41:52 -0500

    Labor Contract changes

--- contracts/LaborContract.sol
+++ contracts/LaborContract.sol
@@ -2,18 +2,24 @@ pragma solidity ^0.4.15;
 
 contract LaborContract {
 
-	bytes32 public uwcLocation;
+	string contractData;
 	address[] public signees;
 	mapping(address => bool) public isSignee;
 
-	function LaborContract(bytes32 contractLocation){
-		uwcLocation = contractLocation;
+	event LogContractSigned(address indexed signee);
+
+	function LaborContract(string data){
+		contractData = data;
 	}
 
 	// To be completed by the potential future employee, from their uPort identity.
 	function signContract()
 	public
 	returns (bool){
-		
+		require(!isSignee[msg.sender]);
+		signees.push(msg.sender);
+		isSignee[msg.sender] = true;
+		LogContractSigned(msg.sender);
+		return true;
 	}
 }
