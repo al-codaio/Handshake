5846b16fd4911d547345755fd3b24d03509c9bca
Jandrews <jordandrews@hotmail.com>
2017-09-30 17:40:37 -0500

    v1 uPort!!

--- src/ViewContract.js
+++ src/ViewContract.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import LaborContract from './contracts/LaborContract.json'
 
 class ViewContract extends Component {
 
@@ -12,7 +13,48 @@ class ViewContract extends Component {
     }
 
     signContract(){
-      // TODO: Add uPort here! Can use eth address for now
+      let laborContractABI = this.props.appContext.uPortWeb3.eth.contract(LaborContract.abi)
+      let laborContractObj = laborContractABI.at(this.props.match.params.address)
+      laborContractObj.sign((error, txHash) => {
+        if (error) { throw error }
+          this.waitForMined(txHash, { blockNumber: null }, // see next area
+          function pendingCB () {
+            // Signal to the user you're still waiting
+            // for a block confirmation
+            console.log('pending');
+          },
+          function successCB (data) {
+            // Great Success!
+            // Likely you'll call some eventPublisherMethod(txHash, data)
+            console.log('Success');
+            console.log('Made it');
+          }
+        )
+      })
+    }
+
+    waitForMined(txHash, response, pendingCB, successCB){
+      let component = this;
+      if (response.blockNumber) {
+        successCB()
+      } else {
+        pendingCB()
+          component.pollingLoop(txHash, response, pendingCB, successCB)
+      }
+    }
+
+    // Recursive polling to do continuous checks for when the transaction was mined
+    pollingLoop(txHash, response, pendingCB, successCB){
+      let component = this;
+      setTimeout(function () {
+        component.props.appContext.uPortWeb3.eth.getTransaction(txHash, (error, response) => {
+          if (error) { throw error }
+            if (response === null) {
+              response = { blockNumber: null }
+            } // Some ETH nodes do not return pending tx
+            component.waitForMined(txHash, response, pendingCB, successCB)
+        })
+      }, 1000) // check again in one sec.
     }
 
     render(){
@@ -32,6 +74,7 @@ class ViewContract extends Component {
           <p>
             {contract.site}
           </p>
+          <button onClick={() => this.signContract()}>Sign</button>
         </div>
       );
     }
